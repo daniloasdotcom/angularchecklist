afd2e09cca0f28dc1aaa98e47d4c4805f35b0667
Danilo Andrade <danilo_as@live.com>
2024-04-21 07:11:29 -0300

    atualização 21042024

--- src/app/components/youtube02/youtube02.component.ts
+++ src/app/components/youtube02/youtube02.component.ts
@@ -9,23 +9,10 @@ import { YouTubePlayerModule } from '@angular/youtube-player';
   styleUrl: './youtube02.component.css'
 })
 export class Youtube02Component {
-//  videoUrl = 'https://www.youtube.com/watch?v=QIZ9aZD6vs0'
+  //  videoUrl = 'https://www.youtube.com/watch?v=QIZ9aZD6vs0'
 
-apiLoaded = false;
+  apiLoaded = false;
 
-videoId = 'eIaG5A0IG50';
-height = 200;
-width = 400;
+  videoId = 'eIaG5A0IG50';
 
-startSeconds = 60;
-endSeconds = 120;
-
-ngOnInit() {
-  if (!this.apiLoaded) {
-    const tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/iframe_api';
-    document.body.appendChild(tag);
-    this.apiLoaded = true;
-  }
-}
 }
