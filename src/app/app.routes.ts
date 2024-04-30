1d2b0a6d32d85a66b7bc85851f59fe8acbb684b1
Danilo Andrade <danilo_as@live.com>
2024-04-29 22:19:23 -0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-
-export const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-    }
-];
