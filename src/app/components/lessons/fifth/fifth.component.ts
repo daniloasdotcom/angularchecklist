18b620fe4634add5d23b28e99070bea4bbc03c8b
Danilo Andrade <danilo_as@live.com>
2024-05-14 17:40:08 -0300

    update14052024

---
+++ src/app/components/lessons/fifth/fifth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fifth',
+  standalone: true,
+  imports: [],
+  templateUrl: './fifth.component.html',
+  styleUrl: './fifth.component.css'
+})
+export class FifthComponent {
+
+}
